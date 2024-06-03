a7f7ff8249aee646983e47cdbe2f4fc6953a6a2a
Kaiji21 <elhaiba120@gmail.com>
2024-06-03 02:31:06 +0100

    Initial commit & Created enseignant,filiere,module,intervention component

---
+++ src/app/filiere/filiere.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filiere',
+  standalone: true,
+  imports: [],
+  templateUrl: './filiere.component.html',
+  styleUrl: './filiere.component.css'
+})
+export class FiliereComponent {
+
+}
---
+++ src/app/intervention/intervention.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intervention',
+  standalone: true,
+  imports: [],
+  templateUrl: './intervention.component.html',
+  styleUrl: './intervention.component.css'
+})
+export class InterventionComponent {
+
+}
